21d54bfffed8dd54744f5f9ec61fbafe968934f9
Tutu Abigail <amaatutu1@gmail.com>
2023-07-12 16:11:31 +0000

    created a stylesheet folder with 5 different style files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./stylesheets/text-elements.css"
+import "./stylesheets/form-elements.css"
+import "./stylesheets/custome-components.css"
+import "./stylesheets/alignments.css"
+import "./stylesheets/theme.css"
+
+function App() {
+ 
+
+  return (
+    <div>
+      Wallet App
+    </div>
+  );
+}
+
+export default App
